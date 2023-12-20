1f1120d0505bb59259f52f9b8266431147c7d8ef
dadesign0709 <dadesign0709@gmail.com>
2023-12-20 17:36:47 +0900

    feat : 로그인 페이지 구현, 유효성 검사 구현

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Celebrity from "./pages/Celebrity";
 import NotFound from "./pages/NotFound";
 import Header from "./components/Header";
 import LoginPage from "./pages/LoginPage";
+import MovieDetail from "./pages/MovieDetail";
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route path='/' element = {<Home/>} />
           <Route path='/movie' element = {<Movies/>} />
+          <Route path='/movie/:title' element = {<MovieDetail/>} />
           <Route path='/tv' element = {<Tv/>} />
           <Route path='/person' element = {<Celebrity/>} />
           <Route path="/login" element = {<LoginPage/>} />
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
-const IMG_BASE_URL = "https:/image.tmdb.org/t/p/w1280";
+export const IMG_BASE_URL = "https://image.tmdb.org/t/p/w1280/";
 
-export default function Movie({title, poster_path, vote_average}) {
+export default function Movie(props) {
+  const navigate = useNavigate();
+
+  const onClickMovieItem = () =>{
+    navigate(`/movie/${props.title}`, {
+      state: props
+    })
+  }
+
   return (
-    <div className='movie-container'>
-        <img src ={IMG_BASE_URL + poster_path} alt='영화포스터'/>
+    <div className='movie-container'  onClick={onClickMovieItem} style={{cursor: 'pointer'}}>
+        <img src ={IMG_BASE_URL + props.poster_path} alt='영화포스터'/>
         <div className='movie-info'> 
-            <h4 className=''>{title}</h4>
-            <span>{vote_average}</span>
+            <h4>{props.title}</h4>
+            <span>{props.vote_average}</span>
         </div>
     </div>
   )
